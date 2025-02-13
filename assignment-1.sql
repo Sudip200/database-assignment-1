e74ab5ac0831602e7ac2bfd077ae32493249a034
root <root@debian.sudiptodeb.org>
2025-02-13 23:31:13 +0530

    adding assignment1 and assignment2

--- assignment-1.sql
+++ assignment-1.sql
@@ -39,51 +39,60 @@ CREATE TABLE OrderDetails (
 );
 -- 7. Insert sample data into Customers table
 INSERT INTO Customers (Name, Email, Address, Phone) VALUES
-('Sudipto Das', 'sudi@example.com', '123 Elm Street', '934238890'),
-('Susovon Mondol', 'susoe@example.com', '456 Maple Avenue', '87723783'),
-('Jaydeep', 'jay@example.com', '789 Oak Lane', '983479344'),
-('Rohit', 'rohit@example.com', '321 Pine Road', '773712379'),
-('Rishav', 'rish@example.com', '654 Cedar Street', '99378839');
+('Sudipto Das', 'sudi@gmail.com', 'West Bengal', '9342388900'),
+('Susovon Mondol', 'susoe@gmail.com', 'West Bengal', '8772378300'),
+('Jaydeep', 'jay@gmail.com', 'Gujrat', '9834793440'),
+('Rohit', 'rohit@gmail..com', 'West Bengal', '7737123790'),
+('Rishav', 'rish@gmail.com', 'Bihar', '9937883960');
 -- 8. Insert sample data into Products table
 INSERT INTO Products (ProductName, Category, Price, Stock) VALUES
-('Laptop', 'Electronics', 999.99, 10),
-('Smartphone', 'Electronics', 699.99, 25),
-('Desk Chair', 'Furniture', 149.99, 30),
-('Bluetooth Headphones', 'Accessories', 199.99, 15),
-('Coffee Maker', 'Appliances', 89.99, 20);
+('Laptop', 'Electronics', 9999.90, 10),
+('Smartphone', 'Electronics', 6999.90, 25),
+('Desk Chair', 'Furniture', 1499.90, 30),
+('Bluetooth Headphones', 'Accessories', 1999.90, 15),
+('Coffee Maker', 'Appliances', 899.90, 20);
 -- 9. Insert sample data into Orders table
 INSERT INTO Orders (CustomerId, OrderDate, TotalAmount) VALUES
-(1, '2025-02-12 10:00:00', 1699.98),
-(2, '2025-02-12 11:30:00', 299.98),
-(3, '2025-02-12 14:15:00', 89.99),
-(4, '2025-02-12 15:00:00', 1199.98),
-(5, '2025-02-12 16:45:00', 1399.98);
+(1, '2025-02-07 10:00:00', 16999.80),
+(2, '2025-02-08 11:30:00', 2999.80),
+(3, '2025-02-09 14:15:00', 899.90),
+(4, '2025-02-10 15:00:00', 11999.80),
+(5, '2025-02-11 16:45:00', 13999.80);
 -- 10. Insert sample data into OrderDetails table
+--subtotals are calculated as Price * Quantity
 INSERT INTO OrderDetails (OrderID, ProductID, Quantity, SubTotal) VALUES
-(1, 1, 1, 999.99),
-(1, 2, 1, 699.99),
-(2, 3, 2, 299.98),
-(3, 5, 1, 89.99),
-(4, 1, 1, 999.99),
-(4, 4, 1, 199.99),
-(5, 2, 2, 1399.98);
+(1, 1, 1, 9999.90),
+(1, 2, 1, 6999.90),
+(2, 3, 2, 2999.80),
+(3, 5, 1, 899.90),
+(4, 1, 1, 9999.90),
+(4, 4, 1, 1999.90),
+(5, 2, 2, 13999.80);
+
 -- 11. Retrieve orders for a specific customer (CustomerID = 1)
+
 SELECT C.Name AS CustomerName, P.ProductName, O.OrderDate, OD.Quantity
 FROM Orders O
 JOIN Customers C ON O.CustomerId = C.CustomerId
 JOIN OrderDetails OD ON OD.OrderId = O.OrderId
 JOIN Products P ON OD.ProductId = P.ProductId
 WHERE C.CustomerId = 1;
+
 -- 12. Find the most purchased product based on total quantity sold
+
 SELECT P.ProductName, SUM(OD.Quantity) AS TotalQuantitySold
 FROM OrderDetails OD
 JOIN Products P ON P.ProductId = OD.ProductId 
 GROUP BY P.ProductId
 ORDER BY TotalQuantitySold DESC
-LIMIT 1;
+LIMIT 1; 
+
+
 -- 13. Update stock quantity after an order is placed for a specific product (ProductID = 1)
+
 UPDATE Products 
-SET Stock = Stock - (SELECT SUM(Quantity) FROM OrderDetails WHERE ProductId = 1)
+SET Stock = Stock - (SELECT SUM(Quantity) FROM OrderDetails WHERE ProductId = 1) --this will update the stock quantity after an order is placed
 WHERE ProductId = 1;
+
 -- 14. Delete a customer record (CustomerID = 5) while maintaining referential integrity
-DELETE FROM Customers WHERE CustomerID = 5;
+DELETE FROM Customers WHERE CustomerID = 5; --this will delete the customer record with CustomerID = 5 without violating referential integrity
--- assignment-2.sql
+++ assignment-2.sql
@@ -4,8 +4,11 @@ SELECT C.Name ,C.CustomerId ,SUM(O.totalamount) AS TotalPurchase  FROM Customers
 O.CustomerId = C.CustomerId GROUP BY C.CustomerId ORDER BY TotalPurchase DESC  LIMIT 3
 
 --Show monthly sales revenue for the last 6 months using PIVOT.
+-- as pivot is not supported in postgresql so using case when 
 SELECT 
     P.ProductName,
+    --sum function to calculate total amount for product name for each month by extracting month from order date 
+    -- and comparing with current date by subtracting 5,4,3,2,1 and if month is current month then sum total amount
     SUM(CASE WHEN EXTRACT(MONTH FROM O.OrderDate) = EXTRACT(MONTH FROM CURRENT_DATE) - 5 THEN O.TotalAmount ELSE 0 END) AS "Month_1",
     SUM(CASE WHEN EXTRACT(MONTH FROM O.OrderDate) = EXTRACT(MONTH FROM CURRENT_DATE) - 4 THEN O.TotalAmount ELSE 0 END) AS "Month_2",
     SUM(CASE WHEN EXTRACT(MONTH FROM O.OrderDate) = EXTRACT(MONTH FROM CURRENT_DATE) - 3 THEN O.TotalAmount ELSE 0 END) AS "Month_3",
@@ -32,105 +35,127 @@ AS ranked_products WHERE ranked_products.RankOfProduct=2;
 --Task 2: Stored Procedures and Functions
 
 CREATE OR REPLACE FUNCTION create_order(customerId INT, productArray INT[], quantityArray INT[])
-RETURNS INT AS $$
+--create order function that takes customerId, productArray, quantityArray as input and returns orderID
+RETURNS INT AS $$ --function body
 DECLARE
-    order_ID INT;
-    i INT;
+    order_ID INT;--variable to store orderID
+    i INT; --variable to iterate over productArray
 BEGIN 
+   --insert into orders table and store orderID in order_ID total amount is 0 initially for the order
     INSERT INTO Orders (CustomerId, TotalAmount) 
     VALUES (customerId, 0)
     RETURNING OrderID INTO order_ID;
-
+    --iterate over productArray
     FOR i IN 1 .. array_length(productArray, 1) LOOP
+        --insert into orderdetails table with orderID, productID, quantity and subtotal
         INSERT INTO OrderDetails (OrderID, Quantity, ProductId, Subtotal)
         VALUES (order_ID, quantityArray[i], productArray[i], 
-                (SELECT Price FROM Products WHERE ProductId = productArray[i]));
+                (SELECT Price FROM Products WHERE ProductId = productArray[i])*quantityArray[i]);
+        --update stock in products table
         UPDATE Products 
         SET Stock = Stock - quantityArray[i]
         WHERE ProductId = productArray[i] 
         AND Stock >= quantityArray[i];
+        --if stock is insufficient then raise exception
         IF NOT FOUND THEN 
             RAISE EXCEPTION 'Insufficient Stock';
         END IF;
     END LOOP;
+    --update total amount in orders table calculated from orderdetails table
     UPDATE Orders 
     SET TotalAmount = (SELECT SUM(Subtotal) FROM OrderDetails WHERE OrderID = order_ID)
     WHERE OrderID = order_ID;
     RETURN order_ID;
 END;
-$$ LANGUAGE plpgsql;
+--end of function body
+$$ LANGUAGE plpgsql; --function language
+--call the function with customerId=3, productArray=[1,2,3], quantityArray=[1,1,1]
 SELECT create_order(3,ARRAY[1,2,3],ARRAY[1,1,1]);
 
 
+
+
+--Function to get total amount spent by a customer
 CREATE OR REPLACE FUNCTION getCustomerSpent(Customer_ID INT)
 RETURNS INT AS $$
 DECLARE 
-total_spent INT;
+total_spent INT; --variable to store total spent
 BEGIN 
+--select sum of total amount from orders table for a customer
   SELECT SUM(o.totalAmount) INTO total_spent FROM Orders o JOIN Customers c ON o.CustomerId= c.CustomerId WHERE c.CustomerId = Customer_Id;
   RETURN total_spent;
 END;
 
 $$ LANGUAGE plpgsql
 
-SELECT getCustomerSpent(4);
+SELECT getCustomerSpent(4); --call the function with CustomerId=4
+
+
+
+
 
 -- Task 3: Transactions and Concurrency Control
+/* 
+    Write a transaction to ensure an order is placed only if all products are in stock. If any product is out of stock, rollback the transaction.
+    Demonstrate how to handle deadlocks when updating order details.
+    Use SAVEPOINT to allow partial updates in an order process where only some items might be out of stock.
+*/
 
-
+--Function to create order with transaction and retry mechanism
 CREATE OR REPLACE FUNCTION create_order_with_transaction(customerId INT, productArray INT[], quantityArray INT[])
 RETURNS INT AS $$
 DECLARE
-    order_ID INT;
-    i INT;
-    _price DECIMAL(10,2);
-    _stock INT;
-    totalAmt DECIMAL(10,2) := 0;
-    is_transaction_successfull BOOLEAN := FALSE;
-    retry INT := 0;
+    order_ID INT; --variable to store orderID
+    i INT; --variable to iterate over productArray
+    _price DECIMAL(10,2); --variable to store price
+    _stock INT; --variable to store stock
+    totalAmt DECIMAL(10,2) := 0; --variable to store total amount
+    is_transaction_successfull BOOLEAN := FALSE; --variable to store transaction status
+    retry INT := 0; --variable to store retry count
 BEGIN
    
            -- SAVEPOINT start_order;
-
+           -- insert into orders table and store orderID in order_ID total amount is 0 initially for the order
             INSERT INTO Orders (CustomerId, TotalAmount)
             VALUES (customerId, 0)
             RETURNING OrderID INTO order_ID;
-
+           -- iterate over productArray
             FOR i IN 1 .. array_length(productArray, 1) LOOP
                 SELECT Stock, Price INTO _stock, _price FROM Products WHERE ProductId = productArray[i] FOR UPDATE;
-
-                IF _stock < quantityArray[i] THEN
+            
+                IF _stock < quantityArray[i] THEN --if stock is insufficient then raise exception
                     RAISE EXCEPTION 'Out of Stock for Product ID %', productArray[i];
-                    ROLLBACK;
-                    CONTINUE;
+                    ROLLBACK; --rollback transaction
+                    CONTINUE; -- continue to next iteration
                 END IF;
-
+              -- insert into orderdetails table with orderID, productID, quantity and subtotal
                 INSERT INTO OrderDetails (OrderID, Quantity, ProductId, Subtotal)
                 VALUES (order_ID, quantityArray[i], productArray[i], _price * quantityArray[i]);
-
+              -- update stock in products table
                 UPDATE Products
                 SET Stock = Stock - quantityArray[i]
                 WHERE ProductId = productArray[i];
-
+              -- update total amount
                 totalAmt := totalAmt + (_price * quantityArray[i]);
-
+              -- if reached last iteration then set transaction status to true
                 is_transaction_successfull := TRUE;
             END LOOP;
-
+            -- if transaction is successfull then update total amount in orders table
             IF NOT is_transaction_successfull THEN
                 ROLLBACK;
                 RAISE EXCEPTION 'Order cancelled due to stock issues';
             END IF;
-
+            -- update total amount in orders table
             UPDATE Orders
             SET TotalAmount = totalAmt
             WHERE OrderID = order_ID;
-
+            
             RETURN order_ID;
 
         EXCEPTION
+         -- handle serialization failure (deadlock) means retry the transaction for 3 times
             WHEN serialization_failure THEN
-                IF retry < 3 THEN
+                IF retry < 4 THEN
                     retry := retry + 1;
                     ROLLBACK;
                 ELSE
@@ -140,11 +165,21 @@ BEGIN
 
 $$ LANGUAGE plpgsql;
 
-SELECT create_order_with_transaction(3,ARRAY[1,2,3],ARRAY[1,1,1000]);
+SELECT create_order_with_transaction(3,ARRAY[1,2,3],ARRAY[1,1,1000]); --result will be out of stock for product 3
+
+
+
+
 
 --Task 4: SQL for Reporting and Analytics
 
 --Generate a customer purchase report using ROLLUP that includes:
+
+-- customer purchase report using ROLLUP that includes:
+-- CustomerName, ProductName, Quantity, Subtotal, GrandTotal
+-- GrandTotal is the total amount spent by the customer
+-- Subtotal is the total amount spent on a specific product
+-- Quantity is the total quantity of a specific product purchased by the customer
 
 SELECT C.Name AS CustomerName,P.ProductName, SUM(OD.Quantity) AS Quantity,SUM(OD.Subtotal) AS Subtotal,SUM(O.totalamount) AS GrandTotal 
 FROM Orders O
@@ -156,11 +191,18 @@ ORDER BY (C.Name,P.ProductName);
 
 -- Use window functions (LEAD, LAG) to show how a customer's order amount compares to their previous order amount.
 
+--customer's order amount compares to their previous order amount using LEAD and LAG
+--CustomerName, OrderDate, CurrentAmount, PreviousAmount, NextAmount
+--CurrentAmount is the total amount of the current order
+--PreviousAmount is the total amount of the previous order
+--NextAmount is the total amount of the next order
+
 SELECT C.Name AS CustomerName,  O.OrderDate,O.TotalAmount AS CurrentAmount ,LAG(O.TotalAmount) OVER (
 PARTITION BY O.CustomerID ORDER BY O.OrderDate) AS PreviousAmount,
 LAG(O.TotalAmount) OVER (
 PARTITION BY O.CustomerID ORDER BY O.OrderDate) AS NextAmount
 FROM Orders O
 JOIN Customers C ON O.CustomerId = C.CustomerId ORDER BY C.CustomerId ,O.OrderDate;
+
 
 
